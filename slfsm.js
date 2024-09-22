9a70e76a317042db7ea930ca69246c1b99452703
NotTeganQuinn-ope <notteganquinn@aol.com>
2024-09-22 15:43:53 -0500

    Create slfsm.js

---
+++ slfsm.js
@@ -0,0 +1,37 @@
+// Vendetta Plugin to Restore Swipe Left for Member List
+import { registerPlugin } from '@vendetta/plugin';
+import { findByProps } from "@vendetta/metro";
+import { getByName } from "@vendetta/metro/filters";
+import { before } from "@vendetta/patcher";
+
+const Drawer = findByProps("openDrawer", "closeDrawer");
+const MembersList = getByName("GuildChannelMembers");
+
+let patcher;
+
+function swipeLeftListener() {
+    // Function to handle swipe left
+    if (Drawer?.openDrawer && MembersList) {
+        Drawer.openDrawer("MemberList", {
+            render: MembersList,
+        });
+    }
+}
+
+export default registerPlugin({
+    name: 'Restore Swipe Left for Member List',
+    description: 'Restores the swipe left gesture to open the member list in Discord.',
+    version: '1.0.0',
+    authors: [{ name: 'YourName', id: 'YourID' }],
+    
+    onLoad() {
+        // Patch the swipe gesture to trigger swipeLeftListener when a left swipe is detected
+        patcher = before('handleGesture', Drawer, swipeLeftListener);
+    },
+
+    onUnload() {
+        // Remove the swipe gesture patch
+        if (patcher) patcher();
+    
+}
+});
