7ad2680c6a538e23d7f95c2f97d8c18c31a197a7
NotTeganQuinn-ope <notteganquinn@aol.com>
2024-09-22 16:23:29 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+export default {
+  onLoad() {
+    console.log("Swipe Left for Member List Plugin Loaded!");
+  },
+  onUnload() {
+    console.log("Swipe Left for Member List Plugin Unloaded!");
+  }
+};
